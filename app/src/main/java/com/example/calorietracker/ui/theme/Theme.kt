c7d7fa987a95a5a12d057e1ad4ca4d85a51559c2
TaxiKab119 <cs.k.alexanderbalan@gmail.com>
2023-07-10 19:23:55 -0400

    first commit

---
+++ app/src/main/java/com/example/calorietracker/ui/theme/Theme.kt
@@ -0,0 +1,49 @@
+package com.example.calorietracker.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+
+private val DarkColorPalette = darkColorScheme(
+    primary = BrightGreen,
+    outlineVariant = DarkGreen,
+    secondary = Orange,
+    background = MediumGray,
+    onBackground = TextWhite,
+    surface = LightGray,
+    onSurface = TextWhite,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+)
+
+private val LightColorPalette = lightColorScheme(
+    primary = BrightGreen,
+    outlineVariant = DarkGreen,
+    secondary = Orange,
+    background = Color.White,
+    onBackground = DarkGray,
+    surface = Color.White,
+    onSurface = DarkGray,
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+)
+
+@Composable
+fun CalorieTrackerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
